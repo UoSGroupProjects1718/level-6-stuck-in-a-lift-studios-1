62e929fd356a5104816b6e85dfd7878da288392a
Zachari Cooper <s178707@ucs.ac.uk>
2017-11-17 16:37:12 +0000

    Level Work
    Copied the game files into my area to safely work on the level.
    Started work on the level Layout

---
+++ Zachari/GoNuts Networked Level Build/Assets/Scripts/UI/Lobby/Player/RemoteEntryUI.cs
@@ -0,0 +1,38 @@
+﻿using Player.SyncedData;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Lobby.Player {
+	public class RemoteEntryUI : MonoBehaviour, EntryInterface {
+
+		public GameObject isReadyBackground;
+		public GameObject isServerBackground;
+		public Text nameText;
+
+		private PlayerDataForClients settings;
+
+		public void SetPlayerObject(GameObject player){
+			settings = player.GetComponent<PlayerDataForClients>();
+
+			UpdateNameFromSettings(player, settings.GetName());
+			UpdateReadyFlagFromSettings(player, settings.GetIsReadyFlag());
+			UpdateServerFlagFromSettings(player, settings.GetIsServerFlag());
+
+			settings.OnNameUpdated += UpdateNameFromSettings;
+			settings.OnIsReadyFlagUpdated += UpdateReadyFlagFromSettings;
+			settings.OnIsServerFlagUpdated += UpdateServerFlagFromSettings;
+		}
+
+		public void UpdateNameFromSettings(GameObject player, string name){
+			nameText.text = name;
+		}
+
+		public void UpdateReadyFlagFromSettings(GameObject player, bool isReady){
+			isReadyBackground.SetActive(isReady);
+		}
+
+		public void UpdateServerFlagFromSettings(GameObject player, bool isServer){
+			isServerBackground.SetActive(isServer);
+		}
+	}
+}
