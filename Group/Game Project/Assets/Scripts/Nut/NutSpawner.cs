66e74258ba51436206e93df920c4efacedc4f90c
Rebecca Brannum <historymaker118@googlemail.com>
2018-01-15 14:36:19 +0000

    Partial fix for the nut dupe bug
    To work as expected, still need to adjust the nut spawn prefabs in engine, but I don't have access to do so right now.

--- Group/Game Project/Assets/Scripts/Nut/NutSpawner.cs
+++ Group/Game Project/Assets/Scripts/Nut/NutSpawner.cs
@@ -1,23 +1,24 @@
-﻿using UnityEngine;
+﻿using Player.Tracking;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Nut {
 	public class NutSpawner : NetworkBehaviour {
 
 		public GameObject nutPrefab;
+		public string nutSpawnTag = "NutSpawn";
 
 		private bool hasSpawned = false;
 
 		public override void OnStartServer () {
 			if (!hasSpawned){
 				hasSpawned = true;
-				GameObject[] spawnLocations = GameObject.FindGameObjectsWithTag("NutSpawn");
-				foreach (GameObject spawnPoint in spawnLocations) {
-					GameObject nut = (GameObject)Instantiate(nutPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+				GameObject[] spawnLocations = GameObject.FindGameObjectsWithTag(nutSpawnTag);
+				for (int i = 0; i < PlayerTracker.GetPlayers().Count; i++){
+					GameObject nut = (GameObject)Instantiate(nutPrefab, spawnLocations[i].transform.position, spawnLocations[i].transform.rotation);
 					NetworkServer.Spawn(nut);
 				}
 			}
 		}
-
 	}
 }
