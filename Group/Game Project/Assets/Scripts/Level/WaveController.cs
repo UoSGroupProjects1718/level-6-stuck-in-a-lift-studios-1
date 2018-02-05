c0b40d731a234839e9827ab445d8cee964a96e3d
limejuice123 <s175883@ucs.ac.uk>
2018-02-05 10:19:15 +0000

    Placed more decoration in scene
    All flagpoles are now readjusted. Added some new hanging baskets. Also placed water in scene, and attempted to write a script to control the waves as the prefab does not come with one. Currently WIP. Lighting data has not changed.

---
+++ Group/Game Project/Assets/Scripts/Level/WaveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour 
+{
+	float scale = 10.0f;
+	float speed = 1.0f;
+	Vector3[] baseHeight;
+
+
+	void Update () 
+	{
+		var mesh = GetComponent<MeshFilter> ().mesh;
+
+		if (baseHeight == null)
+			baseHeight = mesh.vertices;
+
+		var vertices = new Vector3[baseHeight.Length];
+
+		for (int i = 0; i < vertices.Length; i++) 
+		{
+			var vertex = baseHeight [i];
+			vertex.y += Mathf.Sin (Time.time * speed + baseHeight [i].x + baseHeight [i].y + baseHeight [i].z) * scale;
+			vertices [i] = vertex;
+		}
+
+		mesh.vertices = vertices;
+		mesh.RecalculateNormals();
+	}
+}
